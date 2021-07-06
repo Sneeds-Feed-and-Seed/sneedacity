d1d42d44430badf9337825f8c684976f9f2e4240
DevElonCoin <80606032+DevElonCoin@users.noreply.github.com>
2021-07-06 08:49:10 +0200

    Create ubuntu_compile.sh

---
+++ ubuntu_compile.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+sudo apt install pip
+pip3 install conan
+export PATH="/home/$USER/.local/bin:$PATH"
+git clone https://github.com/Sneeds-Feed-and-Seed/sneedacity
+mkdir sneed_build
+cd sneed_build
+cmake -G "Unix Makefiles" -Dsneedacity_use_ffmpeg=loaded ../sneedacity
+make -j$(nproc)
