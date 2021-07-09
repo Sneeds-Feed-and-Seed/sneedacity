817421b9d4644d06bae0729d90a13e6e32fc7fb1
ChucksConsulting <chucksconsulting@chucksconsulting>
2021-07-09 02:59:23 -0300

    Add debug prints function

---
+++ src/Debug.h
@@ -0,0 +1,11 @@
+#ifndef __SNEEDACITY_DEBUGH__
+#define __SNEEDACITY_DEBUGH__
+
+#include <string>
+
+using namespace std;
+
+inline bool global_debug_prints_enabled;
+void dprintf(string message);
+
+#endif
